5ac5b47d4cac08b68712352056159be2bcccbc89
Lalit M <lalitm1004@gmail.com>
2024-02-04 11:17:26 +0530

    revert selectdestination

--- components/SelectDestination/SelectDestination.js
+++ components/SelectDestination/SelectDestination.js
@@ -37,39 +37,6 @@ export default function SelectDestination({ setDestination }) {
   }, [isLoading, error]);
 
   return (
-    <div className="select-destination">
-      {isLoading && (
-        <Card>
-          <CardBody className="flex flex-col p-10">
-            <div className="flex flex-col justify-center items-center">
-              <Spinner color="secondary" labelColor="secondary" size="lg" className="mt-2"/>
-              <p className="text-2xl mt-8">loading destination waypoints</p>
-            </div>
-          </CardBody>
-        </Card>)}
-      {!isLoading && !error && (
-        <div className="flex flex-col items-center justify-center">
-          <select id="select-destination"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-3 w-unit-5xl text-center"
-          >
-            {destinations.map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.name}
-              </option>
-            ))}
-          </select>
-          <Button
-            id="submit-destination"
-            size="md"
-            className="mb-8"
-          >
-            Navigate
-          </Button>
-        </div>)
-      }
-    </div>
-  );
-  (
     <div>
         {isLoading && <p>Loading destinations...</p>}
         {error && <p>Error fetching destinations: {error.message}</p>}
@@ -86,6 +53,6 @@ export default function SelectDestination({ setDestination }) {
           </div>
         )}
       </div>
-  )
+  );
 }
 
